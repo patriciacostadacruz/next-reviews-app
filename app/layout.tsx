eb59c494e75bb8fccc35f9e81efd5247c5af8a16
patriciacostadacruz <patriciacstcz@gmail.com>
2023-08-10 11:32:31 +0200

    Adds footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,13 +19,19 @@ export default function RootLayout({ children }: RootProps) {
                 <Link href="/reviews">Reviews</Link>
               </li>
               <li>
+                {/* argument prefetch={false} can be added to the Link - allows to avoid prefecthing the doc in prod builds */}
                 <Link href="/about">About</Link>
               </li>
             </ul>
           </nav>
         </header>
         <main>{children}</main>
-        <footer>[footer]</footer>
+        <footer>
+          Game data and images, courtesy of:{' '}
+          <a href="https://rawg.io/" target="blank">
+            RAWG
+          </a>
+        </footer>
       </body>
     </html>
   );
