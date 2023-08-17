28ddde300b84cb5555e2891ddcc2d2665622b865
patriciacostadacruz <patriciacstcz@gmail.com>
2023-08-17 10:15:17 +0200

    Adds responisveness to home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 import Heading from '@/components/Heading';
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
     <>
       <Heading>Indie Gamer</Heading>
-      <p>Only the best indie games, reviewed for you.</p>
+      <p className="pb-3">Only the best indie games, reviewed for you.</p>
+      <div className="border bg-white w-80 sm:w-full rounded hover:shadow-xl">
+        <Link
+          className="flex flex-col sm:flex-row"
+          href="/reviews/hollow-knight"
+        >
+          <img
+            src="/images/game.png"
+            alt="Game"
+            width="320"
+            height="180"
+            className="rounded-t sm:rounded-l sm:rounded-r-none"
+          />
+          <h2 className="py-1 text-center font-orbitron font-semibold sm:px-2">
+            Hollow Knight
+          </h2>
+        </Link>
+      </div>
     </>
   );
 }
