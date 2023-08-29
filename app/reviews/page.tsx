474ab7bc0c2b8eb6af4a886c70cf6ef4f658bcf9
patriciacostadacruz <patriciacstcz@gmail.com>
2023-08-29 12:35:40 +0200

    Lists all reviews based on md files and display list in reviews page

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -9,34 +9,25 @@ export default async function ReviewsPage() {
     <>
       <Heading>Reviews</Heading>
       <ul className="flex flex-col gap-3">
-        <li className="border w-80 bg-white rounded hover:shadow-xl">
-          <Link href="/reviews/hollow-knight">
-            <img
-              src="/images/game.png"
-              alt="Game"
-              width="320"
-              height="180"
-              className="rounded-t"
-            />
-            <h2 className="py-1 text-center font-orbitron font-semibold">
-              Hollow Knight
-            </h2>
-          </Link>
-        </li>
-        <li className="border w-80 bg-white rounded hover:shadow-xl">
-          <Link href="/reviews/stardew-valley">
-            <img
-              src="/images/game.png"
-              alt="Game"
-              width="320"
-              height="180"
-              className="rounded-t"
-            />
-            <h2 className="py-1 text-center font-orbitron font-semibold">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
+        {reviews.map((review) => (
+          <li
+            className="border w-80 bg-white rounded hover:shadow-xl"
+            key={review.slug}
+          >
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt="Game"
+                width="320"
+                height="180"
+                className="rounded-t"
+              />
+              <h2 className="py-1 text-center font-orbitron font-semibold">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
