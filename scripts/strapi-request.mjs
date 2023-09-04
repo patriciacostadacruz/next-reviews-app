120d20f681eb8061f987323425a5c49eef670341
patriciacostadacruz <patriciacstcz@gmail.com>
2023-09-04 13:14:25 +0200

    Adds QS package to be able to retireve specific fields from CMS

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,7 +1,16 @@
 import { writeFileSync } from 'node:fs';
+import qs from 'qs';
 
 // populate is needed for CMS to add images and media data
-const url = 'http://localhost:1337/api/reviews' + '?populate=*';
+const url =
+  'http://localhost:1337/api/reviews' +
+  '?' +
+  qs.stringify(
+    {
+      fields: ['slug', 'title'],
+    },
+    { encodeValuesOnly: true }
+  );
 const response = await fetch(url);
 const body = await response.json();
 const formatted = JSON.stringify(body, null, 2);
