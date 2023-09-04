009f1eafb803e2b9e43f2d6e4f68bb972b449954
patriciacostadacruz <patriciacstcz@gmail.com>
2023-09-04 10:10:21 +0200

    Adds state to manage share link button state and change its text onclick

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -2,9 +2,14 @@
 // will be rendered both in server AND browser - hydration
 'use client';
 
+import { useState } from 'react';
+
 export default function ShareLinkButton() {
+  const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
-    return;
+    setClicked(true);
+    setTimeout(() => setClicked(false), 1500);
   };
 
   return (
@@ -12,7 +17,7 @@ export default function ShareLinkButton() {
       onClick={handleClick}
       className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
     >
-      Share link
+      {clicked ? 'Link copied!' : 'Share link'}
     </button>
   );
 }
