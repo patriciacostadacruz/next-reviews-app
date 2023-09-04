58e55bcdfe1e892eef617e70dbd5af6187626364
patriciacostadacruz <patriciacstcz@gmail.com>
2023-09-04 10:49:20 +0200

    Adds heroicons to share link

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -1,3 +1,4 @@
+import { LinkIcon } from '@heroicons/react/20/solid';
 import { useState } from 'react';
 
 export default function ShareLinkButton() {
@@ -12,8 +13,9 @@ export default function ShareLinkButton() {
   return (
     <button
       onClick={handleClick}
-      className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
+      className="border px-2 py-1 flex gap-1 items-center rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
     >
+      <LinkIcon className="h-4 w-4" />
       {clicked ? 'Link copied!' : 'Share link'}
     </button>
   );
