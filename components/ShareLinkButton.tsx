b344928621c100951045680a0f7dd42e7f077de2
patriciacostadacruz <patriciacstcz@gmail.com>
2023-09-04 10:11:31 +0200

    Adds logic to retrieve page URL link

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -8,6 +8,7 @@ export default function ShareLinkButton() {
   const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
+    navigator.clipboard.writeText(window.location.href);
     setClicked(true);
     setTimeout(() => setClicked(false), 1500);
   };
