e8bc15fcfc3584a8adc3251630ab564af8a139f4
patriciacostadacruz <patriciacstcz@gmail.com>
2023-09-05 11:47:04 +0200

    Adds dynamic renderings

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   title: 'Reviews',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default async function ReviewsPage() {
   const reviews = await getReviews(6);
 
