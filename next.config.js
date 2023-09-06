938bddabcf25404eb99c6f8fef732557c9d9f09c
patriciacostadacruz <patriciacstcz@gmail.com>
2023-09-06 13:43:56 +0200

    Adds config image source

--- next.config.js
+++ next.config.js
@@ -12,6 +12,17 @@ module.exports = {
         // from Strapi folder where images come from
         pathname: '/uploads/**',
       },
+      toRemotePattern(process.env.CMS_IMAGE_PATTERN),
     ],
   },
 };
+
+function toRemotePattern(urlString) {
+  const url = new URL(urlString);
+  return {
+    protocol: url.protocol.replace(':', ''),
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+  };
+}
