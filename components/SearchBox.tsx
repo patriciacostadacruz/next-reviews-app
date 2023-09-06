5f84b3de0716ad9967a07c6bc3f15fd7ffc0e7f9
patriciacostadacruz <patriciacstcz@gmail.com>
2023-09-06 13:07:12 +0200

    Adds server-only to allow all our calls to be avail only on server

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import useIsClient from '@/lib/hooks';
-import { SearchableReview, searchReviews } from '@/lib/reviews';
+import { SearchableReview } from '@/lib/reviews';
 import { Combobox } from '@headlessui/react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
@@ -14,7 +14,10 @@ export default function SearchBox() {
   useEffect(() => {
     if (query.length > 1) {
       (async () => {
-        const reviews = await searchReviews(query);
+        const response = await fetch(
+          '/api/search?query=' + encodeURIComponent(query)
+        );
+        const reviews = await response.json();
         setReviews(reviews);
       })();
     } else {
