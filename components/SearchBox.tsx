e17dc580408dc80426f912a680243ccdb4e60450
patriciacostadacruz <patriciacstcz@gmail.com>
2023-09-06 11:05:47 +0200

    Adds client-only rendering to prevent hydration id issues

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,8 +1,12 @@
 'use client';
 
 import { Combobox } from '@headlessui/react';
+import { useEffect, useState } from 'react';
 
 export default function SearchBox() {
+  const [isClient, setisClient] = useState(false);
+  useEffect(() => setisClient(true), []);
+
   return (
     <Combobox>
       <Combobox.Input placeholder="Search..." />
